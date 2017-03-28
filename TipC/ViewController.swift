bf2e931588bf2766ab7852b504442bb0135613e2
Viraj Patel <patelviraj2@yahoo.com>
2017-03-28 15:03:37 -0400

    Initial Commit

---
+++ TipC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipC
+//
+//  Created by Viraj patel on 3/28/17.
+//  Copyright © 2017 Viraj Devlopment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
